c66eafc07791b202614ee9dbd15d189df5528438
NgQuangHuyit <nguyenquanghuy240803@gmail.com>
2024-03-03 20:35:58 +0700

    add statitics tab

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,5 +27,101 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     clickNarbarItem();
+    
+    // charts create
+    var data1 = {
+        labels: ['0-3', '3-5', '5-7', '7-9', '9-10'],
+        datasets: [{
+            label: 'Số lượng người dùng tham gia mỗi kỳ thi',
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+            data: [300, 400, 800, 289, 100],
+            backgroundColor: '#jjj'
+        }]
+    };
+
+    // Thiết lập và vẽ biểu đồ cột
+    var ctx01 = document.getElementById('myChart01').getContext('2d');
+    var myChart01 = new Chart(ctx01, {
+        type: 'bar',
+        data: data1,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+    
+    var data2 = {
+        labels: ['tham-gia', 'không tham gia'],
+        datasets: [{
+            label: 'Ti le tham gia thi',
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+              ],
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+            data: [80, 20],
+
+        }]
+    };
+
+    var ct2 = document.getElementById('myChart02').getContext('2d');
+    var myChart2 = new Chart(ct2, {
+        type: 'doughnut',
+        data: data2,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            },
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Ti le tham gia thi'
+                },
+                
+            }
+        }
+    });
+
+    var data3 = {
+        labels: ['diem-trung binh'],
+        datasets: [{
+            label: 'diem trung binh',
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+              ],
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+            data: [7.7,2.3],
+
+        }]
+    };
+
+    var ct3 = document.getElementById('myChart03').getContext('2d');
+    var myChart3 = new Chart(ct3, {
+        type: 'doughnut',
+        data: data3,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }, 
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Điểm trung bình của người dùng'
+                }
+            }
+        }
+    });
 
 })
