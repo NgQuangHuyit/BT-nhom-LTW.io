24736d1f191cb6c0de028f98ad531c30ea78c072
NgQuangHuyit <nguyenquanghuy240803@gmail.com>
2024-03-03 11:22:04 +0700

    add page

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function() {
+    function clickNarbarItem() {
+        let navbarItems = document.querySelectorAll('.nav__item');
+        let tabId = 0
+        navbarItems.forEach(item => {
+            item.addEventListener('click', function() {
+                let tabs = document.querySelectorAll('.tab');
+                tabs.forEach(tab => {
+                    tab.classList.remove('tab--active');
+                });
+                if (this.id == 'list-exams') {
+                    tabs[0].classList.add('tab--active');
+                }
+                if (this.id == 'list-users') {
+                    tabs[1].classList.add('tab--active');
+                }
+                if (this.id == 'list-statistics') {
+                    tabs[2].classList.add('tab--active');
+                }
+                navbarItems.forEach(item => {
+                    item.classList.remove('nav__item--active');
+                });
+                this.classList.add('nav__item--active');
+                
+            });
+        });
+    }
+    
+
+    clickNarbarItem();
+})
